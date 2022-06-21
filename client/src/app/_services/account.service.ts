140b2360368f6c64869d1718cae4e7216c783f15
caesar <joaovictorcsatu@gmail.com>
2022-06-21 09:24:05 -0300

    Added login persistence in Client

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,15 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   baseUrl = 'https://localhost:7104/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
   Login(model: any) {
-    return this.http.post(this.baseUrl + 'login', model);
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((res: User) => {
+        const user = res;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentuser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  Logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
