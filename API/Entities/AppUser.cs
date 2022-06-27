d740cf531e99b74ab977eb9adeab80a94116ce82
caesar <joaovictorcsatu@gmail.com>
2022-06-26 22:22:13 -0300

    Added complete user attributes and methods

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using API.Extentions;
+
 namespace API.Entities;
 
 public class AppUser
 {
+    [Key]
     public int Id { get; set; }
 
     public string Username { get; set; } = string.Empty;
@@ -9,5 +13,31 @@ public class AppUser
     public byte[] PasswordHash { get; set; }
 
     public byte[] PasswordSalt { get; set; }
+    
+    public string KnownAs { get; set; }
+    
+    public string Gender { get; set; }
+    
+    public string LookingFor { get; set; }
+    
+    public string Introduction { get; set; }
+    
+    public string Interests { get; set; }
+    
+    public DateTime DateOfBirth { get; set; }
 
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime LastLogged { get; set; } = DateTime.Now;
+
+    public string City { get; set; }
+
+    public string Country { get; set; }
+    
+    public ICollection<Photo> Photos { get; set; }
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    } 
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities;
+
+[Table("Photos")]
+public class Photo
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string URL { get; set; }
+
+    public bool MainPicture { get; set; }
+
+    public string PublicId { get; set; }
+}
