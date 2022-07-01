7d4e6935b5e7441564bb8542caac9cf998d1d864
caesar <joaovictorcsatu@gmail.com>
2022-07-01 10:22:56 -0300

    Added JWT interceptor and removed httpOptions

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -1,13 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/IMember';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token,
-  }),
-};
 
 @Injectable({
   providedIn: 'root',
@@ -18,10 +12,10 @@ export class MemberService {
   constructor(private http: HttpClient) {}
 
   GetMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'list', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'list');
   }
 
   GetMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + username);
   }
 }
